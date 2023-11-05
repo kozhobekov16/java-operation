8e3efe7da8038e8e4ab81b1deccbb04a288e5deb
kozhobekov16 <zhakshylykkozhobekov@gmail.com>
2023-11-05 22:26:51 +0600

    initial

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input first number for multiple: ");
+        int num1 = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Operation: ");
+        String operation = scanner.nextLine();
+        String plus = "+";
+        String minus = "-";
+        String del = "/";
+        String mul = "*";
+        System.out.println("Input second number for multiple: ");
+        int num2 = scanner.nextInt();
+        int res = 0;
+        if (operation.equals(plus)) {
+            res = num1 + num2;
+        } else if (operation.equals(minus)) {
+            res = num1 - num2;
+        } else if (operation.equals(del)) {
+            res = num1 / num2;
+        } else if (operation.equals(mul)) {
+            res = num1 * num2;
+        } else {
+            System.out.println("Wrong operation");
+        }
+        System.out.println("Result: " + res);
+    }
+}
